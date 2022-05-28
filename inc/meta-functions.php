20e4688a0a6ed45a52a8e6a17b427be99b2dea06
Tim Kaye <KTS915@users.noreply.github.com>
2022-05-28 18:12:06 -0400

    Update meta-functions.php

--- inc/meta-functions.php
+++ inc/meta-functions.php
@@ -12,6 +12,6 @@ function delete_relationship_meta( $relationship_id, $meta_key, $meta_value = ''
 	return delete_metadata( 'kts_object_relationship', $relationship_id, $meta_key, $meta_value );
 }
 
-function get_relationship_meta( $relationship_id, $meta_key='', $single = '' ) {
+function get_relationship_meta( $relationship_id, $meta_key ='', $single = '' ) {
 	return get_metadata( 'kts_object_relationship', $relationship_id, $meta_key, $single );
 }
